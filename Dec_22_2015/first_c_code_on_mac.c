18f0d1323a018731bd0be20745c224660b766766
loversInJapan <472757599@qq.com>
2015-12-22 22:38:02 +0800

    first commit

---
+++ Dec_22_2015/first_c_code_on_mac.c
@@ -0,0 +1,16 @@
+//
+//  first_c_code_on_mac.c
+//  
+//
+//  Created by   益康 陈 on 12/22/15.
+//
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("hello Apple, hello Steve\n");
+    printf("haha\n");
+    return 0;
+}
