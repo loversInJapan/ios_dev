6c7a4213a85b68286ce88739b6639e8df63e1acc
loversInJapan <472757599@qq.com>
2016-01-18 11:17:10 +0800

    add a funtion

--- Jan_17/APP-Manager/APP-Manager/ViewController.m
+++ Jan_17/APP-Manager/APP-Manager/ViewController.m
@@ -8,13 +8,6 @@
 
 #import "ViewController.h"
 #import "AppInfo.h"
-
-@interface ViewController ()
-
-@property (nonatomic, strong) NSArray* appList;
-
-@end
-
 // 画九宫格界面
 #define kRowCount  3    // 每一行子视图的个数
 #define kAPPAmount 12   // 大视图中app显示个数
@@ -23,6 +16,13 @@
 //#define kMarginX      // 横向间隔
 #define kMarginY   14   // 纵向间隔
 #define kStartY    40   // 最开始的起始点的Y坐标
+
+@interface ViewController ()
+
+@property (nonatomic, strong) NSArray* appList;
+
+@end
+
 
 @implementation ViewController
 // 懒加载app.plist
@@ -85,9 +85,49 @@
         // 按钮是有状态的，不同的状态可以对应不同的标题。但是按钮的字体是不区分状态的
         download.titleLabel.font = [UIFont systemFontOfSize:12];// 设置button中文本的字体
         // UIButton中的titleLabel是readonly的，表示这个指针不能改，但可以修改titleLabel所指的label中的内容
+
+        download.tag = i;
         [appView addSubview:download];
 
+        [download addTarget:self action:@selector(downloadClick:) forControlEvents:UIControlEventTouchUpInside];
     }
+}
+
+
+- (IBAction)downloadClick:(UIButton*)button
+{
+    CGFloat w = 120;
+    CGFloat h = 40;
+    CGFloat x = self.view.frame.size.width * 0.5 - w * 0.5;
+    CGFloat y = self.view.frame.size.height - 20 - h;
+    UILabel* showDownloadInfo = [[UILabel alloc] initWithFrame:CGRectMake(x, y, w, h)];
+    // 设置背景颜色
+    // colorWithWhite:1.0f alpha:0.2  第一个参数：0表示黑色，1表示白色。第二个参数表示透明度
+    [showDownloadInfo setBackgroundColor:[UIColor colorWithWhite:0.0 alpha:0.1]];
+
+    AppInfo* info = self.appList[button.tag];
+    // 设置文字,字体，即显示方式
+    showDownloadInfo.text = [NSString stringWithFormat:@"downloading\n%@", info.name];
+    showDownloadInfo.numberOfLines = 0; // 不限行数，但需要高度够高
+    showDownloadInfo.textAlignment = NSTextAlignmentCenter;
+    showDownloadInfo.font = [UIFont systemFontOfSize:12];
+
+    // 点击后应该让按钮无效。
+    button.enabled = NO;
+
+    // 设置动画让label渐近渐出, 采用了嵌套动画
+    showDownloadInfo.alpha = 0.0; //先将透明度设为0
+    [UIView animateWithDuration:1.0f animations:^{
+        showDownloadInfo.alpha = 0.7; // 经过一秒后将透明度设为0.1
+    } completion:^(BOOL finished){ // 动画结束之后的动作
+        [UIView animateWithDuration:2.0f animations:^{
+            showDownloadInfo.alpha = 0.0;
+        }completion:^(BOOL finished){
+            [showDownloadInfo removeFromSuperview];
+        }];
+    }];
+
+    [self.view addSubview:showDownloadInfo];
 }
 
 @end
