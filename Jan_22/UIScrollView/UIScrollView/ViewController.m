be34ce286390cb767359c9bdd096c903ddd653bd
loversInJapan <472757599@qq.com>
2016-01-22 21:52:10 +0800

    scroll view

--- Jan_22/UIScrollView/UIScrollView/ViewController.m
+++ Jan_22/UIScrollView/UIScrollView/ViewController.m
@@ -9,14 +9,40 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *minionScroll;
+@property (strong, nonatomic) UIImageView* imageView;
+
+//假设图像是从网络上获取的
+@property (strong, nonatomic) UIImage* image;
 
 @end
 
 @implementation ViewController
+- (UIImageView *)imageView
+{
+    if (_imageView == nil) {
+        _imageView = [[UIImageView alloc] init];
+        [self.minionScroll addSubview:_imageView];
+    }
+    return _imageView;
+}
+- (void)setImage:(UIImage *)image
+{
+    _image = image;
+
+    // 设置图像视图的内容
+    self.imageView.image = image;
+    // 让图像视图根据图像大小自动调整大小
+    [self.imageView sizeToFit];
+    // 告诉scrollview内部内容的实际大小
+    self.minionScroll.contentSize = image.size;
+
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+//    self.minionScroll.contentSize = CGSizeMake(892, 632);
+    self.image = [UIImage imageNamed:@"minion"];
 }
 
 - (void)didReceiveMemoryWarning {
